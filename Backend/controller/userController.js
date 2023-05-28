0532ec0dda7dd424cf631aa1d151656ad58e808c
jangiwalia <jangiwalia83@gmail.com>
2023-05-28 14:14:24 -0700

    add error handler middleware

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,7 +1,19 @@
+// @desc: Controller for create new user
+// @access: Public
+// route: /api/users
+
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
   res.send("User created");
 };
 
+// @desc: Controller for login user
+// @access: Public
+// route: /api/users/login
 const loginUser = (req, res) => {
   res.send("User logged in");
 };
--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 0;
 const app = express();
+const { handleError } = require("./middleware/errorMiddleware");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server" });
 });
 
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(handleError);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
