e973cf3da8f39e28567ba1683d0517448c822b71
jangiwalia <jangiwalia83@gmail.com>
2023-05-29 18:33:25 -0700

    connected database

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 0;
 const app = express();
+const connectDB = require("./config/db");
 const { handleError } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
+connectDB();
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
